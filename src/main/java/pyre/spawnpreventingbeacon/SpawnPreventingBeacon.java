15158075435c06629063b3a8519b55d9f67624ae
Kamil Gronowski <kamil540@gazeta.pl>
2019-01-04 20:26:48 +0100

    Initial commit

---
+++ src/main/java/pyre/spawnpreventingbeacon/SpawnPreventingBeacon.java
@@ -0,0 +1,22 @@
+package pyre.spawnpreventingbeacon;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+import pyre.spawnpreventingbeacon.util.Reference;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class SpawnPreventingBeacon {
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
---
+++ src/main/java/pyre/spawnpreventingbeacon/util/Reference.java
@@ -0,0 +1,7 @@
+package pyre.spawnpreventingbeacon.util;
+
+public class Reference {
+    public static final String MOD_ID = "spawnpreventingbeacon";
+    public static final String NAME = "Spawn Preventing Beacon";
+    public static final String VERSION = "1.0.0";
+}
